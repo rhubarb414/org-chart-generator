cbdb5218ab565762a151dff9adf3d1522f4907f8
rhubarb414 <ctophr.dubois@gmail.com>
2023-02-22 17:05:59 -0800

    justifies nav content center

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -9,7 +9,12 @@ const generateHTML = (data) =>
     </head>
 
     <body>
-    <h1>Hello, Org.</h1>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid justify-content-center"">
+             <span class="navbar-brand mb-0 h1">Org Chart</span>
+            </div>
+        </nav>
+    <h2>Hello, Org.</h2>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
