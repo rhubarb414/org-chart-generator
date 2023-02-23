b8c05a5277c3641abe8dc3c2a922a2ffa3381906
rhubarb414 <ctophr.dubois@gmail.com>
2023-02-22 16:49:15 -0800

    adds generateHTML() to index and bootstrap

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const jest = require("jest");
+const generateHTML = require("./src/generateHTML.js");
+
+//create index.html
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("index.html successfully created.")
+  );
+}
+
+function init() {
+  const HTMLContent = generateHTML();
+  writeToFile("./dist/index.html", HTMLContent);
+}
+
+// Function call to initialize app
+init();
