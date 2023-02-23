6cc0397fc1923096c61b2df7ae26c1b7be859efb
rhubarb414 <ctophr.dubois@gmail.com>
2023-02-23 12:25:42 -0800

    creates questions array for inquirer in index.html

--- index.js
+++ index.js
@@ -3,6 +3,40 @@ const fs = require("fs");
 const jest = require("jest");
 const generateHTML = require("./src/generateHTML.js");
 
+//array of questions for inquirer
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter employee name",
+    validate(value) {
+      if (!value) {
+        return "Name cannot be empty";
+      } else {
+        return true;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Enter employee ID",
+    validate(value) {
+      if (typeof value !== number) {
+        return "ID must be a number";
+      } else {
+        return true;
+      }
+    },
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "Enter employee email",
+  },
+];
+
 //create index.html
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
