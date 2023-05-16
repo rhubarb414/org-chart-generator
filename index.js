c9d516e6fb8f2d80d78598db91b48f52b44c3bab
rhubarb414 <ctophr.dubois@gmail.com>
2023-05-16 07:54:28 -0700

    Remove console log from index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const jest = require("jest");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
 
-const Manager = require("./lib/manager");
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 const employeeList = [];
 
-const generateHTML = require("./src/generateHTML.js");
+const generateHTML = require('./src/generateHTML.js');
 
 //~~~~~~~~~~~~ BEGIN Inquirer prompts ~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -15,37 +15,24 @@ const generateHTML = require("./src/generateHTML.js");
 // Manager questions for inquirer
 const managerQs = [
   {
-    type: "input",
-    name: "name",
-    message: "Enter manager name",
+    type: 'input',
+    name: 'name',
+    message: 'Enter manager name',
     validate(value) {
       if (!value) {
-        return "Manager name cannot be empty";
+        return 'Manager name cannot be empty';
       } else {
         return true;
       }
     },
   },
   {
-    type: "input",
-    name: "id",
-    message: "Enter manager ID",
+    type: 'input',
+    name: 'id',
+    message: 'Enter manager ID',
     validate(value) {
       if (!value) {
-        return "ID cannot be empty";
-      } else {
-        return true;
-      }
-    },
-  },
-
-  {
-    type: "input",
-    name: "email",
-    message: "Enter manager email",
-    validate(value) {
-      if (!value) {
-        return "Email cannot be empty";
+        return 'ID cannot be empty';
       } else {
         return true;
       }
@@ -53,59 +40,59 @@ const managerQs = [
   },
 
   {
-    type: "input",
-    name: "office",
-    message: "Enter manager office number",
+    type: 'input',
+    name: 'email',
+    message: 'Enter manager email',
     validate(value) {
       if (!value) {
-        return "Office number cannot be empty";
+        return 'Email cannot be empty';
+      } else {
+        return true;
+      }
+    },
+  },
+
+  {
+    type: 'input',
+    name: 'office',
+    message: 'Enter manager office number',
+    validate(value) {
+      if (!value) {
+        return 'Office number cannot be empty';
       } else {
         return true;
       }
     },
   },
   {
-    type: "list",
-    name: "addNew",
-    message: "Would you like to add another employee?",
-    choices: ["Yes", "No"],
+    type: 'list',
+    name: 'addNew',
+    message: 'Would you like to add another employee?',
+    choices: ['Yes', 'No'],
   },
 ];
 
 //questions for engineer
 const engineerQs = [
   {
-    type: "input",
-    name: "name",
-    message: "Enter engineer name",
+    type: 'input',
+    name: 'name',
+    message: 'Enter engineer name',
     validate(value) {
       if (!value) {
-        return "Name cannot be empty";
+        return 'Name cannot be empty';
       } else {
         return true;
       }
     },
   },
   {
-    type: "input",
-    name: "id",
-    message: "Enter engineer ID",
+    type: 'input',
+    name: 'id',
+    message: 'Enter engineer ID',
     validate(value) {
       if (!value) {
-        return "ID cannot be empty";
-      } else {
-        return true;
-      }
-    },
-  },
-
-  {
-    type: "input",
-    name: "email",
-    message: "Enter engineer email",
-    validate(value) {
-      if (!value) {
-        return "Email cannot be empty";
+        return 'ID cannot be empty';
       } else {
         return true;
       }
@@ -113,59 +100,59 @@ const engineerQs = [
   },
 
   {
-    type: "input",
-    name: "github",
+    type: 'input',
+    name: 'email',
+    message: 'Enter engineer email',
+    validate(value) {
+      if (!value) {
+        return 'Email cannot be empty';
+      } else {
+        return true;
+      }
+    },
+  },
+
+  {
+    type: 'input',
+    name: 'github',
     message: "Enter engineer's GitHub username",
     validate(value) {
       if (!value) {
-        return "Username cannot be empty";
+        return 'Username cannot be empty';
       } else {
         return true;
       }
     },
   },
   {
-    type: "list",
-    name: "addNew",
-    message: "Would you like to add another employee?",
-    choices: ["Yes", "No"],
+    type: 'list',
+    name: 'addNew',
+    message: 'Would you like to add another employee?',
+    choices: ['Yes', 'No'],
   },
 ];
 
 //questions for intern
 const internQs = [
   {
-    type: "input",
-    name: "name",
-    message: "Enter intern name",
+    type: 'input',
+    name: 'name',
+    message: 'Enter intern name',
     validate(value) {
       if (!value) {
-        return "Name cannot be empty";
+        return 'Name cannot be empty';
       } else {
         return true;
       }
     },
   },
   {
-    type: "input",
-    name: "id",
-    message: "Enter intern ID",
+    type: 'input',
+    name: 'id',
+    message: 'Enter intern ID',
     validate(value) {
       if (!value) {
-        return "ID cannot be empty";
-      } else {
-        return true;
-      }
-    },
-  },
-
-  {
-    type: "input",
-    name: "email",
-    message: "Enter intern email",
-    validate(value) {
-      if (!value) {
-        return "Email cannot be empty";
+        return 'ID cannot be empty';
       } else {
         return true;
       }
@@ -173,30 +160,43 @@ const internQs = [
   },
 
   {
-    type: "input",
-    name: "school",
+    type: 'input',
+    name: 'email',
+    message: 'Enter intern email',
+    validate(value) {
+      if (!value) {
+        return 'Email cannot be empty';
+      } else {
+        return true;
+      }
+    },
+  },
+
+  {
+    type: 'input',
+    name: 'school',
     message: "Enter intern's school name",
     validate(value) {
       if (!value) {
-        return "School name cannot be empty";
+        return 'School name cannot be empty';
       } else {
         return true;
       }
     },
   },
   {
-    type: "list",
-    name: "addNew",
-    message: "Would you like to add another employee?",
-    choices: ["Yes", "No"],
+    type: 'list',
+    name: 'addNew',
+    message: 'Would you like to add another employee?',
+    choices: ['Yes', 'No'],
   },
 ];
 
 const employeePrompt = {
-  type: "list",
-  name: "empType",
-  message: "Choose type of employee",
-  choices: ["Engineer", "Intern"],
+  type: 'list',
+  name: 'empType',
+  message: 'Choose type of employee',
+  choices: ['Engineer', 'Intern'],
 };
 
 //~~~~~~~~~~~~ END Inquirer prompts ~~~~~~~~~~~~~~
@@ -213,12 +213,12 @@ const addManager = () => {
         responses.office
       );
       employeeList.push(newManager);
-      if (responses.addNew === "Yes") {
+      if (responses.addNew === 'Yes') {
         createEngOrInt();
       } else {
         filterEmployees();
         const HTMLContent = generateHTML.concatHTML();
-        writeToFile("./dist/index.html", HTMLContent);
+        writeToFile('./dist/index.html', HTMLContent);
       }
     })
     .catch((err) => console.error(err));
@@ -235,12 +235,12 @@ const addEngineer = () => {
         responses.github
       );
       employeeList.push(newEngineer);
-      if (responses.addNew === "Yes") {
+      if (responses.addNew === 'Yes') {
         createEngOrInt();
       } else {
         filterEmployees();
         const HTMLContent = generateHTML.concatHTML();
-        writeToFile("./dist/index.html", HTMLContent);
+        writeToFile('./dist/index.html', HTMLContent);
       }
     })
     .catch((err) => console.error(err));
@@ -258,12 +258,12 @@ const addIntern = () => {
       );
       employeeList.push(newIntern);
 
-      if (responses.addNew === "Yes") {
+      if (responses.addNew === 'Yes') {
         createEngOrInt();
       } else {
         filterEmployees();
         const HTMLContent = generateHTML.concatHTML();
-        writeToFile("./dist/index.html", HTMLContent);
+        writeToFile('./dist/index.html', HTMLContent);
       }
     })
     .catch((err) => console.error(err));
@@ -272,8 +272,7 @@ const addIntern = () => {
 // Inquirer section for asking whether to add new Engineer or Intern
 const createEngOrInt = () => {
   inquirer.prompt(employeePrompt).then((response) => {
-    console.log("createEngOrInt response = " + response);
-    if (response.empType === "Engineer") {
+    if (response.empType === 'Engineer') {
       addEngineer();
     } else {
       addIntern();
@@ -284,9 +283,9 @@ const createEngOrInt = () => {
 // Calls getRole() object method to sort employees and generates page HTML
 const filterEmployees = () => {
   employeeList.forEach((employee) => {
-    if (employee.getRole() === "Manager") {
+    if (employee.getRole() === 'Manager') {
       generateHTML.addManagerHTML(employee);
-    } else if (employee.getRole() === "Engineer") {
+    } else if (employee.getRole() === 'Engineer') {
       generateHTML.addEngineerHTML(employee);
     } else {
       generateHTML.addInternHTML(employee);
@@ -297,7 +296,7 @@ const filterEmployees = () => {
 // Create index.html
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log("index.html successfully created.")
+    err ? console.log(err) : console.log('index.html successfully created.')
   );
 }
 
